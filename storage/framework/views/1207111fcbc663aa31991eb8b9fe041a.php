a764f284c8846c4ae22f135ac4de9076e1290dd2
officialErnests <ernestspursw@gmail.com>
2026-09-10 14:56:13 +0300

    mainer

---
+++ storage/framework/views/1207111fcbc663aa31991eb8b9fe041a.php
@@ -0,0 +1,4 @@
+<div <?php echo e($attributes->merge(['class' => "h-0 w-full relative"])); ?>>
+    <div class="absolute top-[-1px] left-0 right-0 bottom-0 border-t border-dashed border-neutral-300 dark:border-white/[9%]"></div>
+</div>
+<?php /**PATH /home/n0o0b090lv/DEV/LANG/LARAVEL/NOOB_FYI/vendor/laravel/framework/src/Illuminate/Foundation/Providers/../resources/exceptions/renderer/components/separator.blade.php ENDPATH**/ ?>
